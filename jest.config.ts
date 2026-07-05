5477122481cbd2603b611bc2c5cd1bb8f288bd35
Aidar Zaynutdinov <aidar-dev@mail.ru>
2026-07-05 14:49:36 +0300

    Установка Jest в проект

---
+++ jest.config.ts
@@ -0,0 +1,18 @@
+import type { Config } from 'jest';
+
+const config: Config = {
+  collectCoverage: true,
+  coverageDirectory: 'coverage',
+  coverageProvider: 'v8',
+  preset: 'ts-jest',
+  transform: {
+    '^.+\\.tsx?$': [
+      'ts-jest',
+      {
+        // настройки для ts-jest
+      }
+    ]
+  }
+};
+
+export default config;
