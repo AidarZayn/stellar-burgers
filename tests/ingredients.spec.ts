54965c6acd6c9227c1383fe319832081d4fc1476
Aidar Zaynutdinov <aidar-dev@mail.ru>
2026-07-06 19:18:34 +0300

    Отключил запись HAR файла ингредиентов

--- tests/ingredients.spec.ts
+++ tests/ingredients.spec.ts
@@ -3,7 +3,7 @@ import { test, expect } from '@playwright/test';
 test('Записываем HAR файл ингредиентов', async ({ page }) => {
   await page.routeFromHAR('./e2e/hars/ingredients.har', {
     url: '**/ingredients',
-    update: true
+    update: false
   });
 
   await page.goto('/');
